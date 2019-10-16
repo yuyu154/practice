b15ee1aa02538f1904ecc8e0691b89b0f04540d8
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-10-16 21:35:53 +0900

    Update NewProgram.java

--- src/main/java/NewProgram.java
+++ src/main/java/NewProgram.java
@@ -1,5 +1,5 @@
-public class NewProgram {
-    String produce(int number) {
+public class NewProgram implements Program{
+    public String produce(int number) {
         System.out.println("I'm a new program");
         return Integer.toString(number);
     }
