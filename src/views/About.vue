b8f92a965d54d7ed2355b91aa40ed66e8216b69d
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-11-27 23:21:58 +0900

    router 예제

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <HelloWorld msg="hello hello"></HelloWorld>
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default Vue.extend({
+  name: 'about',
+  components: {
+    HelloWorld,
+  }
+})
+</script>
